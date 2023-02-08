8d185de2321036bf1dcf7d29d298ae5e5563c8cf
chajus1 <chajus1@googlemail.com>
2023-02-08 23:58:15 +0000

    Add project files.

---
+++ PlayingWithInterFaces/IClown.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayingWithInterFaces
+{
+    internal interface IClown
+    {
+        public string FunnyThingIHave { 
+            get { return "big shoes"; }
+        }
+        public void Honk();
+    }
+}
---
+++ PlayingWithInterFaces/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayingWithInterFaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TallGuy tallGuy = new TallGuy() { Height = 76, Name = "Jimmy" };
+            tallGuy.TalkAboutYoruself();
+        }
+    }
+}
